94af37102b58caf192949b456dfcfe59414d6869
noahdrakes <noah.drakes@bison.howard.edu>
2022-04-28 00:25:44 -0400

    code cleanup

--- MULTIPLIER_component.vhdl
+++ MULTIPLIER_component.vhdl
@@ -1,39 +1,5 @@
 
 
-
--- COMBINATIONAL ADDER --
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_signed.all;
-use ieee.numeric_std.all;
-
-
-entity DRAKES_COMBO_ADDER is
-generic(
-WIDTH: natural :=128;
-OUTPUT: natural := 128
-);
-port (
-	Op_A: in std_logic_vector(width-1 downto 0); 
-	Op_B: in std_logic_vector(width-1 downto 0);
-	OP_Q: out std_logic_vector(OUTPUT-1 downto 0)
-);
-end DRAKES_COMBO_ADDER;
-
-architecture combo_add of DRAKES_COMBO_ADDER is
-signal S_D: std_logic_vector(width-1 downto 0);
-begin
-S_D <=OP_A+OP_B;
-
-OP_Q<=S_D;
-end combo_add;
-
-
-
-
-
-
 -- ARITHMETIC LOGIC UNIT (ALU) --
 
 library ieee;
@@ -82,14 +48,12 @@ port(
 		CLK: IN STD_LOGIC;
 		RST: IN STD_LOGIC;
 		EN: IN STD_LOGIC;
-		-- OP_A: in std_logic_vector(width-1 downto 0);
 		OP_Q: out std_logic_vector(width-1 downto 0)
 	);
 end shift_LEFT_128BIT;
 
 architecture behavorial of shift_LEFT_128BIT is
 	
-	signal output: std_logic_vector(width-1 downto 0);
 	signal TEMP_Q: std_logic_vector(width-1 downto 0);
 
 begin 
@@ -127,14 +91,12 @@ port(
 		CLK: IN STD_LOGIC;
 		RST: IN STD_LOGIC;
 		EN: IN STD_LOGIC;
-		-- OP_A: in std_logic_vector(width-1 downto 0);
 		OP_Q: out std_logic_vector(width-1 downto 0)
 	);
 end shift_RIGHT_64BIT;
 
 architecture behavorial of shift_RIGHT_64BIT is
 	
-	signal output: std_logic_vector(width-1 downto 0);
 	signal TEMP_Q: std_logic_vector(width-1 downto 0);
 
 begin 
@@ -157,45 +119,6 @@ begin
 		OP_Q<=TEMP_Q;
 end behavorial;
 
-
-
- -- 128 BIT REGISTER (MCAND)
--- library ieee;
--- use ieee.std_logic_1164.all;
-
--- entity Drakes_128_Bit_Reg is
-
--- GENERIC(
--- 	P :integer:= 128
--- 	);
-
---  PORT(	CLK: IN STD_LOGIC;
--- 	RST: IN STD_LOGIC;
--- 	EN: IN STD_LOGIC;
--- 	OP_A: IN STD_LOGIC_VECTOR(P-1 DOWNTO 0);
--- 	OP_Q: OUT STD_LOGIC_VECTOR(P-1 DOWNTO 0));
--- END Drakes_128_Bit_Reg;
-
--- ARCHITECTURE BEHAVIORAL OF Drakes_128_Bit_Reg IS
--- 	SIGNAL TEMP_Q:STD_LOGIC_VECTOR(P-1 DOWNTO 0);
--- BEGIN
--- REG_CLK:PROCESS(CLK)
--- 	BEGIN
--- 	  IF(CLK='1' AND CLK'EVENT)THEN
--- 	     IF(RST='1')THEN
--- 		 -- INITIAL MCAND VALUE IS 32
--- 		TEMP_Q<="00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000";
--- 	     ELSIF(EN='1')THEN
--- 		TEMP_Q<=OP_A;
--- 	     ELSE
--- 		TEMP_Q<=TEMP_Q;
--- 	     END IF;
--- 	  ELSE
--- 		TEMP_Q<=TEMP_Q;
--- 	  END IF;
--- 	END PROCESS REG_CLK;
--- OP_Q<=TEMP_Q;
--- END BEHAVIORAL;
 
 
 
@@ -235,82 +158,4 @@ REG_CLK:PROCESS(CLK)
 	END PROCESS REG_CLK;
 OP_Q<=TEMP_Q;
 END BEHAVIORAL;
-
-
-
--- 64 BIT REG (MULTIPLIER) --
--- library ieee;
--- use ieee.std_logic_1164.all;
-
--- entity Drakes_64_Bit_Reg is
-
--- GENERIC(
--- 	P :integer:= 64
--- 	);
-
---  PORT(	CLK: IN STD_LOGIC;
--- 	RST: IN STD_LOGIC;
--- 	EN: IN STD_LOGIC;
--- 	OP_A: IN STD_LOGIC_VECTOR(P-1 DOWNTO 0);
--- 	OP_Q: OUT STD_LOGIC_VECTOR(P-1 DOWNTO 0));
--- END Drakes_64_Bit_Reg;
-
--- ARCHITECTURE BEHAVIORAL OF Drakes_64_Bit_Reg IS
--- 	SIGNAL TEMP_Q:STD_LOGIC_VECTOR(P-1 DOWNTO 0);
--- BEGIN
--- REG_CLK:PROCESS(CLK)
--- 	BEGIN
--- 	  IF(CLK='1' AND CLK'EVENT)THEN
--- 	     IF(RST='1')THEN
--- 		 -- INITIAL MULT VALUE IS 6
--- 		TEMP_Q<="0000000000000000000000000000000000000000000000000000000000000110";
--- 	     ELSIF(EN='1')THEN
--- 		TEMP_Q<=OP_A;
--- 	     ELSE
--- 		TEMP_Q<=TEMP_Q;
--- 	     END IF;
--- 	  ELSE
--- 		TEMP_Q<=TEMP_Q;
--- 	  END IF;
--- 	END PROCESS REG_CLK;
--- OP_Q<=TEMP_Q;
--- END BEHAVIORAL;
-
--------------------------------------------------------
--- 			SIGN EXTENDERS THAT ARE NOT NEEDED		--
--------------------------------------------------------
-
-
-
--- 128 BIT SIGN EXTENDER
-
--- library ieee;
--- use ieee.std_logic_1164.all ; 
--- entity DRAKES_SIGN_EXTENDER64_128 is
--- GENERIC(
--- 	P:INTEGER:=128); 
--- port ( 
--- 	OP_A: in std_logic_vector (63 downto 0) ;  
--- 	SEXT64: out std_logic_vector (P-1 downto 0) ); 
--- end DRAKES_SIGN_EXTENDER64_128;
-
--- architecture sel_arch of DRAKES_SIGN_EXTENDER64_128 is
-
--- SIGNAL sOnes :STD_LOGIC_VECTOR(63 downto 0):=(OTHERS=>'1');
--- SIGNAL sZeros :STD_LOGIC_VECTOR(63 downto 0):=(OTHERS=>'0');
--- SIGNAL sQ :STD_LOGIC_VECTOR(P-1 downto 0);
-
--- begin 
--- 	WITH OP_A(4) SELECT
-	
--- 	sQ <= sOnes (63 downto 0) &OP_A(63 downto 0) WHEN '1',
--- 	      sZeros (63 downto 0) &OP_A(63 downto 0) WHEN OTHERS;
--- 	SEXT64 <= sQ;
-
--- end sel_arch;
-
-
-
-
-
 
